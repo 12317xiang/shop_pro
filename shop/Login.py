6f454c07f2f68896f92af38405d2e1255c02ae99
12317xiang <1511972446@qq.com>
2020-08-24 17:33:06 +0800

    修改了

--- shop/Login.py
+++ shop/Login.py
@@ -1,4 +1,5 @@
 #注册
+#1.正常注册，2.注册完了登录，如果登录失败则进行注册，3.注册不能使用已经注册过的用户名
 UserList = []
 def register():
     name = input("请输入用户名：")
@@ -7,23 +8,22 @@ def register():
     if len(name) == 0 or len(pas) == 0 or c_pas != pas:
         print("注册失败，请确认输入的用户信息")
     else:
-        UserList.append([name,pas])
-        print("注册成功，去登录吧")
-        return
         for count in UserList:
-            if UserList[count] == name:
+            if name in count:
                 print("用户名已存在，无法进行重新注册")
-            else:
-                register()
+        UserList.append([name,pas])
+        print("注册成功，去登录吧")       
 register()
 
-#登录
+# 登录
 def login():
     print(UserList)
     name = input("请输入登录用户名：")
-    password = input("请输入登录密码：")
-    if name in UserList and password == UserList[name]['pas']:
-        print("登录成功")
-    else: 
-        print("登录失败")
+    pas = input("请输入登录密码：")
+    for count in UserList:
+        if count[0] == name and count[1] == pas:
+            print("登录成功")
+            break
+        else:
+            print("登录失败")
 login()
