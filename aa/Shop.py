e8aa6856b8ef5c42ce2cb228a2cfac96a4d3aa78
12317xiang <1511972446@qq.com>
2020-08-26 17:56:28 +0800

    1.修改了退出登录之后的业务逻辑 2.写完了修改用户信息

--- aa/Shop.py
+++ aa/Shop.py
@@ -1,32 +1,39 @@
 # 注册
-UserList = []
-def register():
-    name = input("请输入用户名：")
-    pas = input("请输入密码:")
-    c_pas = input("请确认密码:")
+# UserList = []
+# def register():
+#     name = input("请输入用户名：")
+#     pas = input("请输入密码:")
+#     c_pas = input("请确认密码:")
     
-    if len(name) ==0 or len(pas) ==0 or pas != c_pas:
-        print("注册失败")
-    else:
-        for count in UserList:
-            if name in count:
-                print("用户名已存在，请重新输入")
-                break
-        UserList.append([name,pas])
-        print("注册成功")
-        print(UserList)
+#     if len(name) ==0 or len(pas) ==0 or pas != c_pas:
+#         print("注册失败")
+#     else:
+#         for count in UserList:
+#             if name in count:
+#                 print("用户名已存在，请重新输入")
+#                 break
+#         UserList.append([name,pas])
+#         print("注册成功")
+#         print(UserList)
         
-register()
+# register()
 
-def login():
-    name = input("请输入用户名：")
-    pas = input("请输入密码:")
-    for count in UserList:
-        if count[0] == name and count[1] == pas:
-            print("登录成功")
-        else:
-            print("登录失败")
-            register()
-login()
+# def login():
+#     name = input("请输入用户名：")
+#     pas = input("请输入密码:")
+#     for count in UserList:
+#         if count[0] == name and count[1] == pas:
+#             print("登录成功")
+#         else:
+#             print("登录失败")
+#             register()
+# login()
+
+list1 = []
+a = 123
+b = "quw"
+list1.append([a,b])
+print(list1)
+list1.append([])
     
     
--- shop/Login.py
+++ shop/Login.py
@@ -1,6 +1,6 @@
 #注册
 #1.正常注册，2.注册完了登录，如果登录失败则进行注册，3.注册不能使用已经注册过的用户名
-type_one = {1:'注册',2:"登录",3:"退出"} #类型      
+type_one = {1:'注册',2:"登录",3:"退出",4:"修改用户信息"} #类型      
 UserList = []
 #注册
 def register():
@@ -23,6 +23,7 @@ def register():
                 if name in count:
                     print("用户名已存在，注册失败，请重新注册")
                     register()
+                    
 # 登录
 def login():
     print(UserList)
@@ -31,15 +32,45 @@ def login():
     for count in UserList:
         if count[0] == name and count[1] == pas:
             print("登录成功")
+            Xiugai_User()
             break
         else:
             print("登录失败，请重新登录！")
             login()
+            
 #修改用户信息
 #1.首先需要登录，修改用户名和修改密码只能单个存在，
-# def Xiugai_User():
+def Xiugai_User():
+    login()
+    text = input("请选择你需要修改的用户信息! （1修改用户名   2.修改密码）")
+    for count in UserList:
+        if text == "1":
+            # text_old_name = input("请输入原用户名：")
+            # if len(text_old_name) == 0:
+            #     print("原用户名不能为空")
+            # elif text_old_name == count[0]:
+            text_new_name = input("请输入新用户名：")
+            if len(text_new_name) != 0:
+                print('用户名修改成功')
+                UserList.append([text_new_name,login().pas])
+            else:
+                print("新用户名不能为空")
+        elif text == "2":
+            text_old_pas = input("请输入原密码：")
+            if text_old_pas != login().pas:
+                print("输入的密码与原密码不匹配")
+            elif text_old_pas == count[0]:
+                text_new_pas = input("请输入新密码：")
+                c_text_new_pas = input("请再次确认新密码：")
+                if len(text_new_pas) != 0 and text_new_pas == c_text_new_pas:
+                    print('密码修改成功,请重新登录')
+                    UserList.append([login().name,text_new_pas])
+                    login()
+                elif text_new_pas != c_text_new_pas:
+                    print("两次输入的密码不一致")
+                else:
+                    print("新密码输入有误")
     
-
 #判断选择的类型
 def xuanze():
     aa = input("请选择你需要操作的选项！(1:注册   2:登录   3:退出   4:修改用户信息)  ：")
@@ -50,9 +81,14 @@ def xuanze():
     elif aa == '2':
         login()
     elif aa == 3:
-        print("退出登录成功")
+        print("退出登录成功,欢迎回到登录页面")
+        shouye = input("请选择你需要操作的选项！(1:注册   2:登录)  ：")
+        if shouye == 1:
+            register()
+        elif shouye == 2:
+            login()
     elif aa == 4:
-        print("功能正在开发中…………")
+        Xiugai_User()
     else:
         print("输入的类型有误")
 xuanze()
